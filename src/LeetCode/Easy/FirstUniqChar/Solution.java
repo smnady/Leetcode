e69de1aa485495945f65ada1314e91d6f16be51f
smnady <smirnovan5670@gmail.com>
2023-02-25 18:52:52 +0300

    new Solution

--- src/LeetCode/Easy/FirstUniqChar/Solution.java
+++ src/LeetCode/Easy/FirstUniqChar/Solution.java
@@ -15,7 +15,13 @@ public class Solution {
             if (mp.get(s.charAt(i))==1) return i;
         return -1;
     }
-    /*  An alternative, faster solution: 
+    public static void main(String[] args) {
+        String str = "leetcode", str2 = "loveleetcode";
+        String str3 = "aabb";
+        System.out.println(firstUniqChar(str) + " " + firstUniqChar(str2) + " " + firstUniqChar(str3));
+    }
+}
+    /*  An alternative, faster solution:
     public void updateMap(Map<Character,Integer> myMap, char ch) {
         if (!myMap.containsKey(ch)) myMap.put(ch, 1);
         else myMap.put(ch, myMap.get(ch) + 1);
@@ -28,10 +34,3 @@ public class Solution {
         return -1;
     }
     */
-
-    public static void main(String[] args) {
-        String str = "leetcode", str2 = "loveleetcode";
-        String str3 = "aabb";
-        System.out.println(firstUniqChar(str) + " " + firstUniqChar(str2) + " " + firstUniqChar(str3));
-    }
-}
