35faa5429a65c2dc6266a2b738189e139c0c78c7
smnady <smirnovan5670@gmail.com>
2023-02-25 18:40:58 +0300

    new Solution

--- src/LeetCode/Easy/MergeSortedArray/Solution.java
+++ src/LeetCode/Easy/MergeSortedArray/Solution.java
@@ -1,2 +1,21 @@
-package LeetCode.Easy.MergeSortedArray;public class Solution {
+package LeetCode.Easy.MergeSortedArray;
+
+import java.util.Arrays;
+
+public class Solution {
+    public static void merge(int[] nums1, int m, int[] nums2, int n) {
+        int j = 0;
+        for (int i = m; i < m+n; i++) {
+            nums1[i]=nums2[j++];
+        }
+        Arrays.sort(nums1);
+    }
+
+    public static void main(String[] args) {
+        int[] nums1 = new int[] {1,2,3,0,0,0};
+        int[] nums2 = new int[]{2,5,6};
+        int m = 3, n = 3;
+        merge(nums1, m, nums2, n);
+        System.out.println(Arrays.toString(nums1));
+    }
 }
