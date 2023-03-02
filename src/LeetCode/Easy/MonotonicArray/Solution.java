58a151abaa1b837914f57c9d86948385bfe4d8b9
smnady <smirnovan5670@gmail.com>
2023-03-02 15:23:50 +0300

    new Solution

--- src/LeetCode/Easy/MonotonicArray/Solution.java
+++ src/LeetCode/Easy/MonotonicArray/Solution.java
@@ -1,2 +1,32 @@
-package LeetCode.Easy.MonotonicArray;public class Solution {
+package LeetCode.Easy.MonotonicArray;
+
+import java.util.Arrays;
+
+public class Solution {
+    public static boolean isMonotonic(int[] nums) {
+        byte b = -1;
+        if (nums.length<3) return true;
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i]>nums[i-1]) {
+                b = 1;
+                break;
+            } else if (nums[i]<nums[i-1]) {
+                b = 0;
+                break;
+            }
+        }
+        if (b==-1) return true;
+        for (int i = 1; i < nums.length; i++) {
+            if (b==1 && nums[i]<nums[i-1]) return false;
+            if (b==0 && nums[i]>nums[i-1]) return false;
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        int[] test = {1,2,2,3};
+        int[] test2 = {6,5,4,4};
+        int[] test3 = {1,3,2};
+        System.out.println(isMonotonic(test)+"\n"+isMonotonic(test2)+"\n"+isMonotonic(test3));
+    }
 }
