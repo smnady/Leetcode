021ff15f97845e6d185cb894aea7a231331494c9
smnady <smirnovan5670@gmail.com>
2023-03-10 11:34:05 +0300

    new Solution

--- src/LeetCode/Easy/ContainsDuplicateII/Solution.java
+++ src/LeetCode/Easy/ContainsDuplicateII/Solution.java
@@ -1,2 +1,26 @@
-package LeetCode.Easy.ContainsDuplicateII;public class Solution {
+package LeetCode.Easy.ContainsDuplicateII;
+
+import javafx.scene.effect.SepiaTone;
+
+import java.util.*;
+
+import static java.util.Collections.list;
+
+public class Solution {
+    public static boolean containsNearbyDuplicate(int[] nums, int k) {
+        Map<Integer,Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            if (map.containsKey(nums[i]) && Math.abs(i-map.get(nums[i]))<=k) return true;
+            else map.put(nums[i], i);
+        }
+        return false;
+    }
+    public static void main(String[] args) {
+        int[] arr1 = {1,2,3,1};
+        int[] arr2 = {1,2,3,1,2,3};
+        int[] arr3 = {1,2,3,1,2,3,3};
+        System.out.println(containsNearbyDuplicate(arr1, 3));
+        System.out.println(containsNearbyDuplicate(arr2, 2));
+        System.out.println(containsNearbyDuplicate(arr3, 2));
+    }
 }
